cb1bd20e2e4c4445cf7b3c04431918ca9ab7231e
Timothy <msiskazikani@gmail.com>
2024-08-27 10:35:37 +0200

    Creating models (Contact-us, Comment-section and Book-table)

--- GHWEB/models.py
+++ GHWEB/models.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django.db import models
+from time import time
+from datetime import datetime
 
 # Create your models here.
 
@@ -49,3 +52,38 @@ class Special(models.Model):
     food_name=models.CharField(max_length=255)
     description=models.CharField(max_length=1024, blank=True)
     pic=models.ImageField(default='img.png')
+
+class Contact(models.Model):
+    location=models.CharField(max_length=1024)
+    opening_hours=models.TimeField()
+    email=models.EmailField(max_length=254)
+    phone=models.IntegerField(max_length=13)
+
+    def __str__(self):
+        return f"Location: {self.location}, opening_hours:{self.opening_hours.strftime('%H')}, email:{self.email}"
+
+class Comment(models.Model):
+    name=models.CharField(max_length=256)
+    email=models.EmailField(max_length=254)
+    subject=models.CharField(max_length=200)
+    message=models.CharField(max_length=1024)
+
+class Book(models.Model):
+    name = models.CharField(max_length=256)
+    email = models.EmailField(max_length=254)
+    message = models.CharField(max_length=1024)
+    num_pple = models.IntegerField()
+    phone = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+    def booking_summary(self):
+        """Returns a summary of the booking details."""
+        return (f"Booking for {self.name} ({self.email})\n"
+                f"Phone: {self.phone}, Number of People: {self.num_pple}\n"
+                f"Date: {self.date.strftime('%Y-%m-%d')}, Time: {self.time.strftime('%H:%M:%S')}\n"
+                f"Message: {self.message}")
+
+    def __str__(self):
+        return f"Booking: {self.name} on {self.date.strftime('%Y-%m-%d')} at {self.time.strftime('%H:%M:%S')}"
+
