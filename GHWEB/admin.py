bf618f0a20a1096b920ae155fd7eb8884a5aa8b4
Timothy <msiskazikani@gmail.com>
2024-08-27 10:40:36 +0200

    Added the models in admin.py and views.py

--- GHWEB/admin.py
+++ GHWEB/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Gallery,Chef,Waiter,HeroSlide,Special,Events,About
+from .models import Gallery,Chef,Waiter,HeroSlide,Special,Events,About,Contact,Comment,Book
 
 # Register your models here.
 interface_models=[Gallery,HeroSlide,Special,Events]
-other=[Chef,Waiter]
+other=[Chef,Waiter,Contact,Comment,Book]
 
 admin.site.register(interface_models)
 
--- GHWEB/views.py
+++ GHWEB/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Gallery,Chef,Waiter,HeroSlide,Events,Special,About
+from .models import Gallery,Chef,Waiter,HeroSlide,Events,Special,About,Contact,Comment,Book
 import random
 
 rand=random.random()*10
@@ -22,13 +22,22 @@ def index(request):
 
     about=list(About.objects.all())
 
+    contact=list(Contact.objects.all())
+
+    book=list(Book.objects.all())
+
+    comment=list(Comment.objects.all())
+
     context={
         "staff":chefs_and_waiters,
         "gallery":gallery,
         "slides":hero_section_slides,
         "events":event,
         "special":specials,
-        "about us":about
+        "about us":about,
+        "book":book,
+        "contact us":contact,
+        "comment-section":comment
     }
     return render(request,"index.html",context)
 
